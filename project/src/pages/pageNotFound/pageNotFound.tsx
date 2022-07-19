40b00f39e2b5702db88e72cbe03fdbe78621b471
Oleksandr <xander@Oleksandrs-MacBook-Pro.local>
2022-07-19 10:18:42 +0100

    link from 404 to home page added

--- project/src/pages/pageNotFound/pageNotFound.tsx
+++ project/src/pages/pageNotFound/pageNotFound.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+import {AppRoute} from '../../const';
+
 import Header from '../../components/header/header';
+
 
 export default function PageNotFound(): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <Header />
+      <Header isNavHidden />
       <main className="page__main page__main--index">
         <h1 className='visually-hidden'>Cities</h1>
 
@@ -44,8 +48,11 @@ export default function PageNotFound(): JSX.Element {
           </section>
         </div>
         <div className='cities'>
-          <div className='cities__places-container container'>
+          <div className='cities__places-container container cities__page-not-found'>
             <h1>404 Page not found ¯\_(ツ)_/¯</h1>
+            <Link className="cities__page-not-found-link" to={AppRoute.Main}>
+              Return to the main page
+            </Link>
           </div>
         </div>
       </main>
