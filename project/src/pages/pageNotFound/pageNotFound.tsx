1253d72beee7e2f8e13a6fb29c2ad61e23f3e939
Oleksandr <xander@Oleksandrs-MacBook-Pro.local>
2022-07-19 10:20:35 +0100

    typo fix

--- project/src/pages/pageNotFound/pageNotFound.tsx
+++ project/src/pages/pageNotFound/pageNotFound.tsx
@@ -7,7 +7,7 @@ import Header from '../../components/header/header';
 export default function PageNotFound(): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <Header isNavHidden />
+      <Header/>
       <main className="page__main page__main--index">
         <h1 className='visually-hidden'>Cities</h1>
 
