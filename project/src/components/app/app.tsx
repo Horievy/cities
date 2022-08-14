f4ff6c6fd744457ac469596e2ec39abe4e9a9f4c
Oleksandr <xander@Oleksandrs-MBP.Home>
2022-08-14 23:53:09 +0100

    add middleware and redirect action

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import {AppRoute} from '../../const';
-import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import {Offer} from '../../types/mainTypes';
 
 import Main from '../../pages/main/main';
@@ -10,6 +10,8 @@ import Property from '../../pages/property/property';
 import PrivateRoute from '../privateRoute/privateRoute';
 import { useAppSelector } from '../../hooks/reduxHooks';
 import Loader from '../loader/loader';
+import HistoryRouter from '../history-route/history-route';
+import browserHistory from '../../browser-history';
 
 interface AppProps {
   favoriteOffers: Offer[],
@@ -23,7 +25,7 @@ function App({favoriteOffers}: AppProps): JSX.Element {
   }
 
   return (
-    <BrowserRouter>
+    <HistoryRouter history={browserHistory}>
       <Routes>
         <Route
           path={AppRoute.Main}
@@ -52,7 +54,7 @@ function App({favoriteOffers}: AppProps): JSX.Element {
           element={<PageNotFound/>}
         />
       </Routes>
-    </BrowserRouter>
+    </HistoryRouter>
   );
 }
 
--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,12 +1,14 @@
 import {createAction} from '@reduxjs/toolkit';
 import { City, Offer, Review, SortType } from '../types/mainTypes';
-import {AuthorizationStatus} from '../const';
+import {AppRoute, AuthorizationStatus} from '../const';
 
 export const changeCity = createAction<{city: City}>('main/changeCity');
 
 export const changeSortType = createAction<{sortType: SortType}>('main/changeSortType');
 
 export const setPlaces = createAction<{placesList: Offer[]}>('main/setPlaces');
+
+export const redirectToRoute = createAction<AppRoute>('main/redirectToRoute');
 
 export const setPlaceId = createAction<number>('offer/setPlaceId');
 
