d2a622eff638eef8b18c698a3264bf579136cee5
Oleksandr <xander@Oleksandrs-MBP.Home>
2022-08-14 21:26:05 +0100

    show review form for loggedIn users only

--- project/src/components/reviews/reviews.tsx
+++ project/src/components/reviews/reviews.tsx
@@ -1,3 +1,5 @@
+import { AuthorizationStatus } from '../../const';
+import { useAppSelector } from '../../hooks/reduxHooks';
 import { Review } from '../../types/mainTypes';
 import ReviewItem from '../review-item/review-item';
 import ReviewsForm from '../reviews-form/reviews-form';
@@ -6,6 +8,9 @@ interface ReviewsProps {
   reviews: Review[],
 }
 export default function Reviews({reviews}: ReviewsProps) {
+  const {authorizationStatus} = useAppSelector((state) => state);
+  const isLoggedIn = authorizationStatus === AuthorizationStatus.Auth;
+
   return (
     <section className="property__reviews reviews">
       <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
@@ -14,7 +19,7 @@ export default function Reviews({reviews}: ReviewsProps) {
           reviews.map((review) => <ReviewItem key={review.id} review={review}/>)
         }
       </ul>
-      <ReviewsForm />
+      {isLoggedIn && <ReviewsForm />}
     </section>
   );
 }
